b15fe7a2a03f4166087498392ac9a261f7b15353
Quan Tran <quantran@Quans-MacBook-Pro.local>
2020-07-04 20:40:29 +0700

    updated comment

--- redux-basic.js
+++ redux-basic.js
@@ -1,19 +1,17 @@
 const valueEl = document.getElementById("value");
 
-const counter = (state, action) => {
-  if (typeof state === "undefined") return 0;
-  console.log("Previous state", state);
-  console.log("Action type", action.type);
+function counter(state = 0, action) {
   switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
     default:
-      return state;
+      return state
   }
-};
-
+}
+// Create a Redux store holding the state of your app.
+// Its API is { subscribe, dispatch, getState }.
 const store = Redux.createStore(counter);
 
 render = () => {
@@ -23,7 +21,8 @@ render = () => {
 };
 // setup an click event on #increment when the click is trigger tell the store please dispatch increment action
 document.getElementById("increment").addEventListener("click", () => {
-    store.dispatch({ type: "INCREMENT" });
+  // The only way to mutate the internal state is to dispatch an action.  
+  store.dispatch({ type: "INCREMENT" });
 });
 document.getElementById("decrement").addEventListener("click", () => {
     store.dispatch({ type: "DECREMENT" });
